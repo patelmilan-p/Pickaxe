46ac464dfb73ac1455a8a77e14ec02f649b8b867
Hemant Patel <hemant.patel@crowdint.com>
2011-07-22 05:05:11 +0530

    Updated codebreaker (2nd feature passed)

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -3,7 +3,7 @@ end
 
 When /^I start a new game$/ do
   game = Codebreaker::Game.new(output)
-  game.start
+  game.start('1234')
 end
 
 Then /^I should see "([^"]*)"$/ do |message|
@@ -22,4 +22,18 @@ end
 
 def output
   @output ||= Output.new
+end
+
+
+Given /^the secret code is "([^"]*)"$/ do |secret|
+  @game = Codebreaker::Game.new(output)
+  @game.start(secret)
+end
+
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess(guess)
+end
+
+Then /^the mark should be "([^"]*)"$/ do |mark|
+  output.messages.should include(mark)
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,23 +2,61 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:output) { double('output').as_null_object }
+    let(:game)   { Game.new(output) }
+
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output').as_null_object
-        game = Game.new(output)
-
         output.should_receive(:puts).with("Welcome to Codebreaker!")
-        game.start
+        game.start('1234')
       end
 
       it "prompts for the first guess" do
-        output = double('output').as_null_object
-        game = Game.new(output)
-
         output.should_receive(:puts).with("Enter a Guess:")
-        game.start
+        game.start('1234')
+      end
+    end
+
+    describe "#guess" do
+      context "with no matches" do
+        it "sends a mark with ''" do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
       end
 
+      context "with 1 number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+
+      context "with 1 exact match" do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+
+      context "with 2 number matches" do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          output.should_receive(:puts).with('--')
+          game.guess('4355')
+        end
+      end
+
+      context "with 1 number match and 1 exact match (in that order)" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+            output.should_receive(:puts).with('+-')
+            game.guess('2535')
+        end
+      end
     end
   end
 end
